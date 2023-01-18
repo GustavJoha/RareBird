3daf7b030623f214982de969d248776c3616d6f4
HackerMan761 <79506155+HackerMan761@users.noreply.github.com>
2023-01-18 10:57:04 +0100

    Pantmaskin Prefab

--- RareBird26/Assets/Zekes kod/playerljud.cs
+++ RareBird26/Assets/Zekes kod/playerljud.cs
@@ -4,59 +4,35 @@ using UnityEngine;
 
 public class playerljud : MonoBehaviour
 {
-    AudioSource AudioSource;
-    float lastFootstepPlayedTime;
-    Vector3 lastPlayerPosition;
-    float VELOCITY_POLL_PERIOD = 0.2f; // 5 times a second
-    private Vector3 playerPosition;
-    private float stepsPerSecond;
-    private object timePerStep;
-    private float currentFootstepsWaitingPeriod;
+    const float a = 0.24f;
+    const float n = 2.0f;
+    const float b = 1.68f;
+    const float c = 159.0f;
+    AudioSource audioSource;
+    public float playerVelocity;
 
-    public void Start()
+    [SerializeField]
+    public Running_Around spring;
+
+    private void Start()
     {
-        AudioSource = GetComponent<AudioSource>();
-        InvokeRepeating("EstimatePlayerVelocity_InvokeRepeating", 1.0f, VELOCITY_POLL_PERIOD);
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        playerVelocity = 6;
+        float mappedPlayerSpeed = playerVelocity; //Convert the speed so that walking speed is about 6
+        float stepsPerSecond = ((a * Mathf.Pow(mappedPlayerSpeed, n)) + (b * mappedPlayerSpeed) + c) / 60.0f;
+        float timePerStep = (1.0f / stepsPerSecond);
+        float currentFootstepsWaitingPeriod = timePerStep; //Amount of time to wait before playing the next audio source
+        float sound = timePerStep -= Time.deltaTime;
+
+        if (true)
+        {
+            audioSource.Play();
+        }
     }
 
-    public void EstimatePlayerVelocity_InvokeRepeating()
-    {
-
-        float distanceMagnitude = (playerPosition - lastPlayerPosition).magnitude;
-        lastPlayerPosition = playerPosition;
-        float estimatedPlayerVelocity = distanceMagnitude / VELOCITY_POLL_PERIOD;
-        if (estimatedPlayerVelocity < 15.0f)
-        {
-            //Avoid playing footsteps audio for moving at very small speeds
-            //As the human foot moves forward, the head (camera) moves forward before the foot hits the ground
-            //because the current foot on the ground pushes the body forward.
-            currentFootstepsWaitingPeriod = Mathf.Infinity;
-            return;
-        }
-
-        const float a = 0.24f;
-        const float n = 2.0f;
-        const float b = 1.68f;
-        const float c = 159.0f;
-        float mappedPlayerSpeed = estimatedPlayerVelocity / 10.0f; //Convert the speed so that walking speed is about 6
-        stepsPerSecond = ((a * Mathf.Pow(mappedPlayerSpeed, n)) + (b * mappedPlayerSpeed) + c) / 60.0f;
-        timePerStep = (1.0f / stepsPerSecond);
-        currentFootstepsWaitingPeriod = (float)timePerStep;
 
 
-
-
-    }
-        public void Update()
-        {
-
-            if (Time.time - lastFootstepPlayedTime > currentFootstepsWaitingPeriod)
-            {
-
-            //Play another footstep  audio file
-                AudioSource.Play();
-                lastFootstepPlayedTime = Time.time;
-            }
-
-        }
 }
