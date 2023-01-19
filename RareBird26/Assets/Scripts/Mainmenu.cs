23062b7913bb5bdf1163c44ffd9764af0a7cada5
thansson2004 <79506289+thansson2004@users.noreply.github.com>
2023-01-19 13:21:18 +0100

    victor kan inte vissla

--- RareBird26/Assets/Scripts/Mainmenu.cs
+++ RareBird26/Assets/Scripts/Mainmenu.cs
@@ -9,6 +9,9 @@ public class Mainmenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
- 
+    public void quit()
+    {
+        Application.Quit();
+    }
 }
     
