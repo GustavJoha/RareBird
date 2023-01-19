a5e221f721c4e11c5541182b507cfd13ff0b669e
thansson2004 <79506289+thansson2004@users.noreply.github.com>
2023-01-19 13:24:18 +0100

    m

--- RareBird26/Assets/Mainmenu.cs
+++ RareBird26/Assets/Mainmenu.cs
@@ -2,15 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class GameOver : MonoBehaviour
+public class Mainmenu : MonoBehaviour
 {
     private void Start()
     {
         Cursor.lockState = CursorLockMode.None;
-
+    }
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void ReturnToMenu()
     {
         SceneManager.LoadScene("aawaagga");
     }
+    public void quitGame()
+    {
+        Application.Quit();
+    }
 }
+    
